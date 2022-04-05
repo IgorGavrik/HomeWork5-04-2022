5199501737388b3b761dcb7dca0e5c68d9dcd2d4
mac <igor.gavrik@tut.by>
2022-04-05 21:48:30 +0300

    Initial commit

--- HomeWork05_04_2022/HomeWork05_04_2022/urls.py
+++ HomeWork05_04_2022/HomeWork05_04_2022/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import project
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('project/', show_project)
+    path('project/', project)
 ]
--- HomeWork05_04_2022/project/views.py
+++ HomeWork05_04_2022/project/views.py
@@ -2,7 +2,13 @@ from rest_framework.views import APIView
 from django.shortcuts import HttpResponse
 
 
+def post(request):
+    print(request.body)
+    print("Query", request.GET)
+    print("Body", request.POST)
+    return  HttpResponse()
+
+
 class TestView(APIView):
-    def project(self, request):
+    def post(self, request):
         return HttpResponse()
-    
